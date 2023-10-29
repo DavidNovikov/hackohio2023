fd113afd8a1b59a671b3e688110a2b78271ea418
Alex Gulko <alexgulko99@gmail.com>
2023-10-28 21:55:41 -0400

    UI update

--- client/renderer.js
+++ client/renderer.js
@@ -4,3 +4,21 @@ const func = async () => {
 }
 
 func()
+
+Split(['#item-gallery', '#menu'], {
+  minSize: 300,
+  sizes: [75, 25],
+})
+
+const items = document.querySelectorAll('.item')
+
+for (const item of items) {
+  item.addEventListener('click', () => {
+    if (item.classList.contains('selected')) {
+      item.classList.remove('selected')
+    } else {
+      document.querySelector('.item.selected')?.classList.remove('selected')
+      item.classList.add('selected')
+    }
+  })
+}
